10c02059eceb61102d18d0b54bf575008c43b96e
sateesh.pagolu <sateesh.pagolu@xero.com>
2020-04-12 20:13:29 +1000

    flip card on keypress

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import "./styles/App.css";
 import Header from "./components/Header/Header.js";
 import Cards from "./components/Cards/Cards.js";
 import React from "react";
-
+import { useEffect } from 'react';
+import {getIdForLetter} from './helpers/utils.js'
+import { useDispatch } from "react-redux";
+import { SetFlipStatus } from "./actions";
+import * as Sides from './data/sides.js'
 
 const App = () => {
+
+  //const happyPress = useKeyPress('h');
+  const dispatch = useDispatch();
+
+  function downHandler({ key }){
+    let idOfTheKeyPressed = getIdForLetter(key);
+
+    if(idOfTheKeyPressed === null){
+      console.log("invalid key pressed!");
+    }
+    else{
+      dispatch(SetFlipStatus({ id: idOfTheKeyPressed, side: Sides.imageSide }));
+    }
+
+  }
+
+  useEffect(()=> {
+    window.addEventListener('keydown', downHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+    };
+
+  }, [])
+
+
   return (
     <div>
       <Header/>
